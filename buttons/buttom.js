48b3f774ada40152bef18cb5105108f8bd4fb987
Vamsi Akkala <vamsi.a@zelarsoft.com>
2021-10-19 11:46:23 +0530

    c

---
+++ buttons/buttom.js
@@ -0,0 +1,14 @@
+function button()
+{ 
+	var ph=document.getElementById("s").value
+
+	var	phone=ph.length
+
+if(phone>0)
+	{
+	document.getElementById("p").removeAttribute("disabled")
+	}
+	else
+	{
+		document.getElementById("p").setAttribute("disabled","true")
+	}
